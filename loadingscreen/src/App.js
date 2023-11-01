ae4ee33e707acf35ff886886a0c5c9ff0a41d135
Lamboserker <lukaslamberz96@gmail.com>
2023-11-01 22:29:56 +0100

    changes

--- loadingscreen/src/App.js
+++ loadingscreen/src/App.js
@@ -1,74 +1,78 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
+import {
+  bolt,
+  champagne,
+  flask,
+  joint,
+  money,
+  pills,
+  skull,
+  smoking,
+} from "./assets/svg/index";
 
 function App() {
+  const videoRef = useRef(null);
   const [loading, setLoading] = useState(0);
-  const [showElements, setShowElements] = useState(false);
   const [isMusicPlaying, setIsMusicPlaying] = useState(false);
-  const [randomWord, setRandomWord] = useState("");
-  const [wordPosition, setWordPosition] = useState({ top: 0, left: 0 });
-  const [opacity, setOpacity] = useState(0);
+  const [showElements] = useState(false);
+  const [iconIndex, setIconIndex] = useState(0); // Zustand zum Verfolgen des aktuellen Icons
+  const icons = [bolt, champagne, flask, joint, money, pills, skull, smoking]; // Liste der Icons
+ 
 
-  const words = ["Zähle Socken", "Sammel Blätter", "Drehe Joint", "Lade Waffe"];
+  const playMusicAndStartLoading = () => {
+    playMusic();
+    startLoading();
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+  
 
   const playMusic = () => {
-    const audio = new Audio("http://localhost:3000/music");
+    const audio = new Audio("http://localhost:4000/music");
     audio.play();
     setIsMusicPlaying(true);
-    setTimeout(() => {
-      setShowElements(true);
-    }, 2000);
   };
 
-  const generateRandomWordAndPosition = useCallback(() => {
-    const randomWord = words[Math.floor(Math.random() * words.length)];
-    const randomTop = Math.floor(Math.random() * 100);
-    const randomLeft = Math.floor(Math.random() * 100);
-    setRandomWord(randomWord);
-    setWordPosition({ top: `${randomTop}vh`, left: `${randomLeft}vw` });
-    setOpacity(0);
-  }, [words]);
+  const startLoading = () => {
+    let interval = setInterval(() => {
+      setLoading((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return 100;
+        }
+        return prev + 1;
+      });
+    }, 100);
+  };
 
+  // Effect zum Wechseln der Icons
   useEffect(() => {
-    let interval;
+    let iconInterval;
     if (isMusicPlaying) {
-      interval = setInterval(() => {
-        setLoading((prevLoading) => {
-          if (prevLoading >= 100) {
-            clearInterval(interval);
-            return 100;
-          }
-          return prevLoading + 1;
-        });
-      }, 100);
+      iconInterval = setInterval(() => {
+        setIconIndex((prevIndex) => (prevIndex + 1) % icons.length); // Icon wechseln
+      }, 500); // Intervall für den Wechsel der Icons
     }
-    return () => clearInterval(interval);
+    return () => clearInterval(iconInterval);
   }, [isMusicPlaying]);
 
-  useEffect(() => {
-    let interval;
-    if (isMusicPlaying) {
-      interval = setInterval(() => {
-        generateRandomWordAndPosition();
-        setOpacity(1);
-      }, 2000);
-    }
-    return () => clearInterval(interval);
-  }, [isMusicPlaying, generateRandomWordAndPosition]);
-
-  useEffect(() => {
-    let timeout;
-    if (opacity === 1) {
-      timeout = setTimeout(() => {
-        setOpacity(0);
-      }, 1000); // 1 Sekunde nach dem Wort erscheint
-    }
-    return () => clearTimeout(timeout);
-  }, [opacity]);
 
   return (
     <div className="App">
-      {!isMusicPlaying && <button onClick={playMusic}>Start</button>}
+       <button onClick={playMusicAndStartLoading}>Start</button>
+
+      <video ref={videoRef} loop muted className="background-video">
+        <source
+          src="https://cdn.discordapp.com/attachments/1075140494973218846/1169333453234380932/bg_2.mp4?ex=655505a4&is=654290a4&hm=5d75201df0de106de8d78d5bb7044ac6ec83c9e53c791b2399ccd25a36ab9551&"
+          type="video/mp4"
+        />
+      </video>
+     
+      <div className="icon-container">
+     
+    </div>
       <div className={`elements ${showElements ? "slide-in" : ""}`}>
         <div id="load">
           <div>G</div>
@@ -80,18 +84,7 @@ function App() {
           <div>L</div>
         </div>
       </div>
-      <div
-        className="random-word"
-        style={{
-          position: "absolute",
-          top: wordPosition.top,
-          left: wordPosition.left,
-          opacity: opacity,
-          transition: "opacity 1s ease-in-out",
-        }}
-      >
-        {randomWord}
-      </div>
+    
       <div className="loading-bar">
         <div className="loading-fill" style={{ width: `${loading}%` }}></div>
       </div>
