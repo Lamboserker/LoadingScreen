b1638caa72ced7b24c980bcadddcb1d59ee2792a
Lukas Lamberz <120056355+Lamboserker@users.noreply.github.com>
2023-11-04 15:17:42 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,16 +6,16 @@ description 'Custom loadingscreen for fiveM'
 version '1.0.0'
 loadscreen_cursor 'yes'
 loadscreen_manual_shutdown 'no'
-loadscreen 'html/index.html'
+loadscreen 'index.html'
 
 files {
-    'html/index.html',
-    'html/assets/CSS/main.css',
-    'html/assets/JS/main.js',
+    'index.html',
+    'html/assets/css/main.css',
+    'html/assets/js/main.js',
     
     'html/assets/videos/background.mp4', 
-    'html/assets/music/onlymp3.to - Kendrick Lamar M.A.A.D. City Feat. MC eiht -10yrPDf92hY-192k-1698776278.mp3' 
+    'html/assets/music/Kendrick Lamar-M.A.A.D.City.mp3' 
 }
 
 -- Server-sided music streaming might require additional server configuration
-loadscreen_music 'html/assets/music/onlymp3.to - Kendrick Lamar M.A.A.D. City Feat. MC eiht -10yrPDf92hY-192k-1698776278.mp3'
+loadscreen_music 'html/assets/music/Kendrick Lamar-M.A.A.D.City.mp3'
